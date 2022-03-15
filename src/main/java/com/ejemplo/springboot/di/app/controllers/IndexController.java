ae5df02495502df4b0872cee14e4d3c5ebbf9643
steeltx <realsteel.gnu@gmail.com>
2022-03-14 23:36:46 -0600

    Implementando Autowired

--- src/main/java/com/ejemplo/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/ejemplo/springboot/di/app/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.ejemplo.springboot.di.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,8 @@ import com.ejemplo.springboot.di.app.models.service.MiServicio;
 @Controller
 public class IndexController {
 	
-	private MiServicio servicio = new MiServicio(); 
+	@Autowired
+	private MiServicio servicio; 
 	
 	@GetMapping({"/","","/index"})
 	public String index(Model model) {
--- src/main/java/com/ejemplo/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/ejemplo/springboot/di/app/models/service/MiServicio.java
@@ -1,5 +1,8 @@
 package com.ejemplo.springboot.di.app.models.service;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MiServicio {
 	
 	public String operacion() {
